581fe457550df1b7f6a06d24755e5fedaadede38
ufiv <lingranhuang@gmail.com>
2023-10-25 02:35:07 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Kafka } from "@upstash/kafka";
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		// auth check
+		if (request.method != 'POST') {
+			return new Response("no auth", { status: 403 });
+		}
+		const param = JSON.parse(await request.text());
+		if (param.pass != env.PASS) {
+			return new Response(param, { status: 403 });
+		}
+		const kafka = new Kafka({
+			url: env.UPSTASH_KAFKA_REST_URL,
+			username: env.UPSTASH_KAFKA_REST_USERNAME,
+			password: env.UPSTASH_KAFKA_REST_PASSWORD,
+		});
+		const topic = "sms";
+		if (new URL(request.url).pathname == "/send") {
+			// send msg
+			const msg = param.msg;
+			const p = kafka.producer();
+			ctx.waitUntil(p.produce(topic, msg));
+			const data = {
+				code: 200,
+				msg: "ok!"
+			}
+			return new Response(JSON.stringify(data), { status: 200 });
+		} else if (new URL(request.url).pathname == "/get") {
+			// get msg
+			const c = kafka.consumer();
+			const messages = await c.consume({
+				consumerGroupId: "group_1",
+				instanceId: "instance_1",
+				topics: ["sms"],
+				autoOffsetReset: "earliest",
+			});
+			// return new Response(JSON.stringify(messages, null, 2));
+			const data = messages.map(item => {
+				const parts = item.value.split("\n");
+				const o = {
+					sender: parts[0],
+					time: parts[1],
+					msg: parts.slice(2).join("\n")
+				};
+				return o;
+			});
+		
+			return new Response(JSON.stringify(data));
+		}
+		else {
+			return new Response("no auth", { status: 403 });
+		}
 	},
 };
